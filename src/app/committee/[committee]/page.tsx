022da2215c3df210738319938d01ceff5928b3d6
Uncool <106897936+Uncool-dudes@users.noreply.github.com>
2025-01-29 09:09:50 +0530

    Fixed committee memeber card and tracks yet to be added

--- src/app/committee/[committee]/page.tsx
+++ src/app/committee/[committee]/page.tsx
@@ -14,13 +14,13 @@ export default async function CommitteePage({ params }: { params: Promise<{ comm
   }
 
   return (
-    <div className="absolute inset-0 w-full bg-[#d5b173] py-8">
-      <div className="max-w-8xl mx-auto px-4 md:px-6">
+    <div className="absolute inset-0 w-full bg-[#d5b173] py-8 overflow-y-scroll">
+      <div className="max-w-8xl mx-auto px-4 md:px-6 flex flex-col items-center justify-center">
         <h1 className="text-3xl md:text-4xl font-black text-[#1f2d36] text-center mb-8">
           {roles[committeeKey]}
         </h1>
 
-        <div className="flex flex-row justify-center gap-6">
+        <div className="w-fit flex flex-col xl:flex-row justify-center gap-6">
           {CommitteeMembers.filter((member) =>
             member.role.includes(committeeKey)
           ).map((member) => (
